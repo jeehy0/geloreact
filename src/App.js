5e3d4f5f92251261c4ebe2f3b7422c243f8a1edf
jeehy0 <gelofilomeno13@gmail.com>
2024-01-24 13:42:00 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+      <Button />
     </div>
   );
 }
 
 export default App;
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>FAST REACT PIZZA CO</h1>
+    </div>
+  );
+}
+
+function Footer() {
+  const time = new Date().getHours;
+  console.log(time);
+
+  if (time >= 12 && time < 22) {
+    return (
+      <footer className="footer" style={{ textAlign: "center", color: "red" }}>
+        We are OPEN. We're open from 12:00 to 22:00. Come visit us or order
+        online.
+      </footer>
+    );
+  } else {
+    return (
+      <footer className="footer" style={{ textAlign: "center", color: "red" }}>
+        We are CLOSED. We're open from 12:00 to 22:00. Come visit us or order
+        online.
+      </footer>
+    );
+  }
+}
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      <p>
+        Authentic Italian Cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+      <div className="pizzas">
+        <Pizza />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+      </div>
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div className="pizza">
+      <img src="./pizzas/focaccia.jpg"></img>
+      <div className="pizza">
+        <h3>Focaccia</h3>
+        <p>Bread with italian olive oil and rosemary.</p>
+        <span>6</span>
+      </div>
+    </div>
+  );
+}
+
+function Button() {
+  return <button className="btn">Order</button>;
+}
